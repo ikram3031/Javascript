ed41a844501cc5521977436806dbc434a918ded3
ikram3031 <mdikram.3031@gmail.com>
2022-03-20 16:54:46 +0600

    20/03

--- Blackjack/bl.js
+++ Blackjack/bl.js
@@ -1,5 +1,5 @@
 let blackjackGame = {
-    'you': {'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0},
+    'you': {'scoreSpan': '#your-blackjack-result', 'div': '#your-box', },
     'dealer': {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0},
     'cards': ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'K', 'J', 'Q', 'A'],
     //assigning values of cards
